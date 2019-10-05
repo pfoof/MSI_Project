e46dadf55bbbc3998d8609af336b88c941c6f6f0
pfoof <rotworm0@yahoo.pl>
2019-10-05 18:53:34 +0200

    some routing added

--- server/src/msiproject/main.go
+++ server/src/msiproject/main.go
@@ -8,7 +8,26 @@ import (
 	"net/http"
 )
 
-func editItem(w http.ResponseWriter, r *http.Request) {
+func listItems(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		item(w, r)
+		return
+	} else if r.Method == "GET" {
+		fmt.Fprintf(w, "\tID\tManufacturer\tName")
+		for i, e := range Entities {
+			fmt.Fprintf(w, "%d:\t%d\t%s\t%s", i, e.ID, e.Prod, e.Name)
+		}
+	} else if r.Method == "HEAD" {
+		w.Header().Add("Content-Length", "1024")
+		w.Header().Add("Content-Type", "text/plain")
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+}
+
+func item(w http.ResponseWriter, r *http.Request) {
 
 	var buffer []byte
 
@@ -45,5 +64,9 @@ func editItem(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", editItem)
+	rtr := mux.NewRouter()
+	http.Handle("/", rtr)
+
+	rtr.HandleFunc("/", listItems)
+	rtr.HandleFunc("/{[0-9]+}", item)
 }
