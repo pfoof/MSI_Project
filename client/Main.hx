feaf37552685ea91d5cba37e716415c13b684c9d
pfoof <rotworm0@yahoo.pl>
2019-10-07 16:52:25 +0200

    Client dir

---
+++ client/Main.hx
@@ -0,0 +1,14 @@
+package;
+
+import js.html.DOMElement;
+import js.Browser;
+import js.html.Window;
+
+class Main {
+
+    public static function main() {
+        var window: Window = new Window();
+        var element: DOMElement = window.elementById("hello");
+    }
+
+}
