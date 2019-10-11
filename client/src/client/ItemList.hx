c52526e5532116a086485837f8bc255b16ad09ad
pfoof <rotworm0@yahoo.pl>
2019-10-11 15:04:23 +0200

    List of items, inserting, refreshing

--- client/src/client/ItemList.hx
+++ client/src/client/ItemList.hx
@@ -1,5 +1,8 @@
 package client;
 
+import haxe.Json;
+import priori.bootstrap.PriBSFormInputText;
+import priori.net.PriRequestMethod;
 import priori.event.PriTapEvent;
 import priori.net.PriURLLoader;
 import priori.net.PriURLRequest;
@@ -14,6 +17,9 @@ class ItemList extends PriGroup {
     
     private var list: PriDataGrid;
     private var refreshButton: PriBSFormButton;
+    private var insertButton: PriBSFormButton;
+    private var insertProd: PriBSFormInputText;
+    private var insertName: PriBSFormInputText;
 
     public function new() {
         super();
@@ -28,12 +34,29 @@ class ItemList extends PriGroup {
 
         addChild(refreshButton);
 
+        insertButton = new PriBSFormButton();
+        insertButton.text = "Insert";
+        insertButton.addEventListener(PriTapEvent.TAP, insert);
+        insertProd = new PriBSFormInputText();
+        insertProd.placeholder = "Manufacturer";
+        insertName = new PriBSFormInputText();
+        insertName.placeholder = "Product";
+
+        addChild(insertName);
+        addChild(insertProd);
+        addChild(insertButton);
+
         list = new PriDataGrid();
         list.columns = [
             new PriGridColumn("ID", "id", PriGridColumnSizeType.FIXED, 48, true),
             new PriGridColumn("Manufacturer", "prod", PriGridColumnSizeType.FIT, false),
             new PriGridColumn("Product", "name", PriGridColumnSizeType.FIT, false)
         ];
+        list.data = [{
+            id: 100,
+            prod: "htc",
+            name: "polaris"
+        }];
         addChild(list);
 
         refresh();
@@ -43,7 +66,13 @@ class ItemList extends PriGroup {
         super.paint();
 
         refreshButton.x = 48;
-        refreshButton.y = 48;
+        refreshButton.y = insertButton.y = insertProd.y = insertName.y = 48;
+        insertButton.x = this.width - 48 - insertButton.width;
+        var inputWidth = (insertButton.x - refreshButton.maxX - 3 * 24) / 2;
+        insertProd.x = 24 + refreshButton.maxX;
+        insertProd.width = inputWidth;
+        insertName.x = 24 + insertProd.maxX;
+        insertName.width = inputWidth;
 
         list.x = 48;
         list.width = this.width - 48*2;
@@ -52,15 +81,34 @@ class ItemList extends PriGroup {
     }
 
     public function refresh(?e: PriEvent): Void {
+        trace("Starting request");
         var request: PriURLRequest = new PriURLRequest("http://localhost:8000/");
+        request.method = PriRequestMethod.GET;
         var loader: PriURLLoader = new PriURLLoader();
         loader.addEventListener(PriEvent.COMPLETE, onLoad);
         loader.load(request);
     }
 
+    public function insert(e: PriEvent): Void {
+        var request: PriURLRequest = new PriURLRequest("http://localhost:8000/");
+        request.method = "POST";
+        request.data = Json.stringify({
+            name: insertName.value,
+            prod: insertProd.value
+        });
+        var requester: PriURLLoader = new PriURLLoader();
+        requester.addEventListener(PriEvent.COMPLETE, onInsert);
+        requester.load(request);
+    }
+
+    public function onInsert(e: PriEvent): Void {
+        refresh();
+    }
+
     public function onLoad(e: PriEvent): Void {
         var dataString: String = e.data;
         list.data = haxe.Json.parse(dataString);
+        trace(dataString);
     }
 
     public function addItem(item: ItemModel) {
